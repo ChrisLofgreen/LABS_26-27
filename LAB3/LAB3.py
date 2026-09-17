d064279a84ded20a5b660c99598fed24e6f76260
ChrisLofgreen <chrislofgreen@live.com>
2026-09-17 12:10:54 +0200

    changed blankout method

--- LAB3/LAB3.py
+++ LAB3/LAB3.py
@@ -3,7 +3,7 @@
 # Part A
 
 # 1.
-""" 
+'''
 number = 0
 
 if number == 0:
@@ -12,10 +12,10 @@ elif number > 0:
     print("positive")
 else:
     print("negative")
- """
+'''
 
 # 2.
-""" 
+'''
 age = input("Print your age: ")
 age = int(age)
 
@@ -27,10 +27,10 @@ elif age > 14:
     print("try to have fun while it lasts!")
 else:
     print("Do remember to listen to your parents some times, they've been through this you know")
- """
+'''
 
 # 3.
-""" 
+''' 
 user = "username"
 password = "P1"
 
@@ -41,10 +41,10 @@ if input_u == user and input_p == password:
     print("Great, you're in!")
 else:
     print("Sorry, wrong credenntials")
-"""
+'''
 
 # 4.
-""" 
+'''
 score = 50
 
 if score in range(90, 101):
@@ -59,10 +59,10 @@ elif score in range(10, 30):
     print ("E")
 else:
     print("YOU'RE OUT OF WACK!")
-"""
+'''
 
 # 5.
-""" 
+'''
 member = False
 total_order = 1000
 
@@ -70,10 +70,10 @@ if total_order > 999 or member == True:
     print("ship!")
 else:
     print("wait for more orders")
-"""
+'''
 
 # 6.
-""" 
+'''
 1 == 1 #True
 1 != 1 #False
 1 > 1 #False
@@ -87,12 +87,12 @@ print(1 > 1)
 print(1 < 1)
 print(1 >= 1)
 print(1 <= 1)
-"""
+'''
 
 # Part B
 
 # 1.
-""" 
+'''
 e_str = ""
 ne_str = " "
 zero = 0
@@ -129,17 +129,17 @@ if ne_list:
     print("list")
 else:
     print("no list")
- """
+'''
 
 # 2.
-""" 
+''' 
 languages = ["Python", "C++", "C"]
 
 print("C" in languages)
 print("C#" in languages)
- """
+'''
 # 3.
-""" 
+'''
 blocked = ["nastyword1", "nastyword2", "nastyword3"]
 
 user_input = input("Please provide your prefered username: ")
@@ -148,10 +148,10 @@ if user_input in blocked:
     print("aha... That won't do dear, and I think you know why")
 else:
     print(f"Great! Your username is {user_input}")
- """
+'''
 
 # 4.
-""" 
+'''
 blocked = ["nastyword1", "nastyword2", "nastyword3"]
 
 user_input = input("Please provide your prefered username: ")
@@ -160,8 +160,8 @@ if user_input not in blocked:
     print(f"Great! Your username is {user_input}")
 else:
     print("aha... That won't do dear, and I think you know why")
- """
-""" 
+ '''
+'''
 great_apes = ["orangutan", "schimpanzee", "gorilla", "bonobo", "homo sapiens"]
 
 ape = "makak"
@@ -172,22 +172,21 @@ elif ape == great_apes[0]:
     print("We've got the greatest of apes!")
 else:
     print("We've got a great ape")
- """
+'''
 
 # Part C
 
 # 1.
-""" 
+'''
 names = ["Hume", "Mill", "Dennett"]
 number = 1
  
 for name in names:
     print(name, ": You are welcome to the annual philosophers conference for the dead! \n You are member nr.", number)
     number += 1
- """
+ '''
 
-# 2.
-""" 
+''' 
 number = 1
 while number <= 50:
     if number % 2 == 0:
@@ -195,20 +194,20 @@ while number <= 50:
         number += 1
     else:
         number += 1
- """
+'''
 
 # 3.
-""" 
+'''
 numbers = [1, 2, 3, 4, 5]
 sum = 0
 for number in numbers:
     sum = sum + number
 
 print(sum)
- """
+'''
 
 # 4.
-""" 
+'''
 numbers = [1, 2, 3, 4, 5, 2]
 largest = 0
 
@@ -217,10 +216,10 @@ for number in numbers:
         largest = number
 
 print(largest)
- """
+'''
 
 # 5.
-""" 
+'''
 words = ["ape", "orangutan", "pig", "dog", "anaconda"]
 above5 = 0
 
@@ -229,10 +228,10 @@ for word in words:
         above5 += 1
 
 print(above5)
- """
+'''
 
 # 6.
-""" 
+'''
 scores = [50, 70, 80, 90, 20, 10]
 passes = 0
 fails = 0
@@ -244,10 +243,10 @@ for score in scores:
 
 print(passes)
 print(fails)
- """
+'''
 
 # 7.
-"""
+'''
 dictionary = {"name" : "Glenn", "food" : "bacon", "age" : 52}
 
 for key in dictionary:
@@ -258,20 +257,20 @@ for key, value in dictionary.items():
 
 for item in dictionary.items():
     print(item)
-"""
+'''
 
 # Part D
 
 # 1.
-""" 
+'''
 number = 10
 
 while number in range(1,11):
     print(number)
     number -=1
- """
+ '''
 # 2.
-""" 
+'''
 user_input = input("enter a number for multiplication: ")
 user_input = int(user_input)
 count = 1
@@ -279,18 +278,18 @@ count = 1
 while count < 10:
     print(count," x ", user_input," = ",user_input * count)
     count += 1
- """
+'''
 
 # 3.
-""" 
+'''
 playlist = ["Song1", "Song2", "Song3", "Song4", "Song5"]
 
 for index, song in enumerate(playlist):
     print(index +1,": ", song)
- """
+'''
 
 # 4.
-""" 
+'''
 x = 1
 y = 1
 
@@ -303,9 +302,9 @@ while x <= 3:
             x += 1
             y = 1
             break
- """
+ '''
 # 5.
-""" 
+''' 
 xcount = 1
 ycount = 1
 
@@ -320,22 +319,22 @@ while xcount <= 5:
 
     print(list1)
     xcount += 1
- """
+ '''
 
 
 # Part E
 
 # 1.
-""" 
+'''
 number = 10
 
 while number in range(0,11):
     print(number)
     number -=1
- """
+ '''
 
 # 2.
-""" 
+'''
 password = "P1"
 user_input = ""
 
@@ -343,20 +342,20 @@ while user_input != password:
     user_input = input("Type password: ")
 
 print("Got it")
- """
+ '''
 
 # 3.
-""" 
+''' 
 quitstatement = "quit"
 user_input = ""
 
 while user_input != quitstatement:
     user_input = input("Type command or 'quit': ")
     print(user_input)
-"""
+'''
 
 # 4.
-""" 
+'''
 counter = int(0)
 user_input = int(1)
 
@@ -366,10 +365,10 @@ while user_input != 0:
     counter = counter + user_input
 
 print(counter)
- """
+ '''
 
 # 5.
-""" 
+'''
 counter = int(0)
 secret = int(5)
 user_input = int(1)
@@ -384,12 +383,12 @@ while user_input != secret:
         print("too low!")
 
 print("Got it!", secret)
- """
+'''
 
 # Part F
 
 # 1.
-""" 
+'''
 number = 1
 
 while number <= 100:
@@ -398,10 +397,10 @@ while number <= 100:
         break
     else:
         number += 1
- """
+'''
 
 # 2.
-""" 
+'''
 strings = ["t1", "t2", "", "t3", "", "t4"]
 
 for string in strings:
@@ -409,10 +408,10 @@ for string in strings:
         continue
     else:
         print(string)
- """
+'''
 
 # 3.
-""" 
+'''
 names = ["Glenn", "Ann", "Ada", "Camille", "Finn"]
 target = "Finn"
 found = False
@@ -426,10 +425,10 @@ if found:
     print("Found target: ", target)
 else:
     print("not found, went through the whole list... nothing")
- """
+ '''
 
 # 4.
-""" 
+'''
 nums = [40, 55, -55, -32, 999, 50]
 
 for num in nums:
@@ -440,7 +439,7 @@ for num in nums:
         break
     else:
         print(num)
- """
+'''
 
 # Part G
 
@@ -471,17 +470,17 @@ studysess = [
 
 
 # 2.
-""" 
+'''
 totaltime = 0
 
 for sess in studysess:
     totaltime = totaltime + sess["minutes"]
 
 print(totaltime)
- """
+'''
 
 # 3.
-""" 
+'''
 emptydict = {}
 sub = ""
 
@@ -493,9 +492,9 @@ for dict in studysess:
             emptydict[sub] += dict["minutes"]
 
 print(emptydict)
- """
+'''
 # 4.
-""" 
+'''
 max = 0
 
 for dict in studysess:
@@ -503,17 +502,17 @@ for dict in studysess:
         max = dict["minutes"]
 
 print(max)
- """
+'''
 
 # 5.
-""" 
+'''
 for dict in studysess:
     if dict["minutes"] > 45:
         print (dict["subject"], dict["minutes"])
- """
+'''
 
 # 6.
-""" 
+'''
 print("")
 print("Welcome to the premier student scruteneering service of 2026!\n(made in close collaboration with Microslop, and a group of anonomous latin american meat-packing glitterati)\n")
 
@@ -558,14 +557,14 @@ while True:
 
     else:
         continue
- """
+'''
 
 
 
 # Part H
 
 # 1.
-""" 
+'''
 counter = 1
 
 while counter <= 100:
@@ -581,10 +580,10 @@ while counter <= 100:
     else:
         print(counter)
         counter += 1
- """
+'''
 
 # 2.
-""" 
+'''
 sentence = input("print a sentence: ")
 sentence = sentence.lower()
 counter = 0
@@ -595,10 +594,10 @@ for char in sentence:
         counter += 1
 
 print(counter)
- """
+'''
 
 # 3.
-""" 
+'''
 list1 = [1, 6, 5, 8, 4, 1, 3, 6, 4, 1, 4]
 duplicates = []
 
@@ -609,12 +608,12 @@ for item in list1:
 
 duplicates = set(duplicates)
 print(duplicates)
- """
+'''
 
 # 4.
-""" 
+'''
 list1 = [3, 5, 2]
 
 for number in list1:
     print ("*" * number)
-"""
+'''

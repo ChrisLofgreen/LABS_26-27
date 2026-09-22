b28e68b0ad2c1255a4961a4889a8843c74f12632
ChrisLofgreen <chrislofgreen@live.com>
2026-09-22 14:08:27 +0200

    added E

--- LAB7/LAB7.py
+++ LAB7/LAB7.py
@@ -2,7 +2,7 @@
 
 
 # PART D
-
+'''
 # 1.
 
 class Student:
@@ -52,3 +52,49 @@ passing_students = [student for
 
 for student in passing_students:
     print (student.name, student.score)
+'''
+
+# PART E
+# 1.
+class Teacher:
+    def __init__(self, name):
+        self.name = name
+# 2.
+class Course:
+    def __init__(self, name, teacher):
+        self.name = name
+        self.teacher = teacher
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+
+# 3.
+teacher1 = Teacher("Anne with an 'e'")
+course1 = Course("Social Skills", teacher1)
+
+# 4.
+print(course1.name, ":",course1.teacher.name)
+
+# 5.
+student1 = Student("Alenka")
+student2 = Student("Slavoj")
+student3 = Student("Friedrich")
+student4 = Student("Ludwig")
+student5 = Student("David")
+student6 = Student("Anne")
+# 6.
+course1.add_student(student1)
+course1.add_student(student2)
+course1.add_student(student3)
+course1.add_student(student4)
+course1.add_student(student5)
+course1.add_student(student6)
+# 7.
+for student in course1.students:
+    print(student.name)

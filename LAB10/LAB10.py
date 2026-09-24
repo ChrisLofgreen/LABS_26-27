a658ab5fbbfe1adef4f0fdfdf99a294b49630108
ChrisLofgreen <chrislofgreen@live.com>
2026-09-24 10:43:05 +0200

    first commit part E

---
+++ LAB10/LAB10.py
@@ -0,0 +1,29 @@
+
+
+
+
+# Part E
+'''
+# 1.
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
+
+
+product1 = Product("Momin cup", 150)
+product2 = Product("Mad hatter's hat", 1500)
+product3 = Product("T-1000's sunglasses", 75)
+
+# print(product1)
+# print(product2)
+# print(product3)
+
+product2_str = str(product2)
+
+print(product2_str)
+'''
